e93cbc63f1c20e47edcec0e842f5367205a166d0
Nipaaaa1 <frostone224@gmail.com>
2026-02-22 23:11:28 +0000

    test: add DummyTest

---
+++ makefile
@@ -0,0 +1,5 @@
+run:
+	./mvnw spring-boot:run
+
+test:
+	./mvnw test
---
+++ src/test/java/com/nipaaaa/chess_matchmaking/DummyTest.java
@@ -0,0 +1,13 @@
+package com.nipaaaa.chess_matchmaking;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+
+  @Test
+  public void shouldPass() {
+    assertTrue(true);
+  }
+}
