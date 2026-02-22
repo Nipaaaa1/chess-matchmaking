b9b8061d14d2f283129d70cb76153526766e9548
Nipaaaa1 <frostone224@gmail.com>
2026-02-22 22:32:02 +0000

    chore: setup project

---
+++ src/main/java/com/nipaaaa/chess_matchmaking/ChessMatchmakingApplication.java
@@ -0,0 +1,13 @@
+package com.nipaaaa.chess_matchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessMatchmakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessMatchmakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nipaaaa/chess_matchmaking/ChessMatchmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipaaaa.chess_matchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessMatchmakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
